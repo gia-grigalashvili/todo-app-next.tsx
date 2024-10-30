d926208061ffef4bf3737b8c7fdb8b07dddf9128
gia-grigalashvili <grigalagia07@gmail.com>
2024-10-30 23:57:14 +0400

    start

---
+++ src/app/inputs/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="flex  flex-col items-center">
+      <h1 className="text-[20px]">todo app</h1>
+      <input className="p-[10px]  bg-[#000000] text-[#ffff]" type="text" />
+    </div>
+  );
+}
